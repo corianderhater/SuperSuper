b79a0537a142a74c029dc3932456253bd41b8bc6
Paweł Unger <42935624+corianderhater@users.noreply.github.com>
2023-04-20 17:51:11 +0200

    Q

--- Speaker/Throat/ThroatWithOffsetCenter.cs
+++ Speaker/Throat/ThroatWithOffsetCenter.cs
@@ -8,59 +8,78 @@ namespace SuperSuper.Speaker.Throat
 {
     public class ThroatWithOffsetCenter : GH_Component
     {
-        /// <summary>
-        /// Initializes a new instance of the ThroatWithOffsetCenter class.
-        /// </summary>
+
+        protected override System.Drawing.Bitmap Icon => null;
+        public override Guid ComponentGuid => new Guid("DBA8CFCC-5611-4B78-A9A6-54A421E1FDFB");
+
         public ThroatWithOffsetCenter()
           : base("ThroatWithOffsetCenter", "Nickname",
               "Description",
-              "Category", "Subcategory")
+              "SuperSuper", "Speakers")
         {
         }
 
-        /// <summary>
-        /// Registers all the input parameters for this component.
-        /// </summary>
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
+            pManager.AddPlaneParameter("Center", "C", "", GH_ParamAccess.item);
+            pManager.AddNumberParameter("CenterOffset", "CO", "", GH_ParamAccess.item);
+            pManager.AddNumberParameter("ThroatRadius", "TR", "", GH_ParamAccess.item);
+            pManager.AddNumberParameter("HorizontalDispersionAngle", "HA", "", GH_ParamAccess.item);
+            pManager.AddNumberParameter("VerticalDispersionAngle", "VA", "", GH_ParamAccess.item);
+            pManager.AddIntegerParameter("Resolution", "R", "", GH_ParamAccess.item);
         }
 
-        /// <summary>
-        /// Registers all the output parameters for this component.
-        /// </summary>
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
+            pManager.AddCurveParameter("C", "C", "", GH_ParamAccess.list);
+            pManager.AddBrepParameter("B", "B", "", GH_ParamAccess.list);
         }
 
-        /// <summary>
-        /// This is the method that actually does the work.
-        /// </summary>
-        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
         protected override void SolveInstance(IGH_DataAccess DA)
+        {
+
+            Plane center = Plane.Unset;
+            double centerOffset = 0.0;
+            double throatRadius = 0.0;
+            double hAngle = 0.0;
+            double vAngle = 0.0;
+            int res = 0;
+
+            if (!DA.GetData(0, ref center)) { return; }
+            if (!DA.GetData(1, ref centerOffset)) { return; }
+            if (!DA.GetData(2, ref throatRadius)) { return; }
+            if (!DA.GetData(3, ref hAngle)) { return; }
+            if (!DA.GetData(4, ref vAngle)) { return; }
+            if (!DA.GetData(5, ref res)) { return; }
+
+
+
+            var circles = new List<Curve>();
+            var angle = 90 - hAngle;
+            var x = GetCircleXPosition(angle, centerOffset, throatRadius);
+            var circleCenter = new Point3d(x, -centerOffset, 0);
+            var line = new Line(center.Origin, center.Origin + new Vector3d(1000, 1000 * Math.Tan(angle), 0));
+            var param = line.ClosestParameter(circleCenter);
+            var PT = new Point3d(center.Origin.X + throatRadius, center.OriginY, center.OriginZ);
+            var r = PT.DistanceTo(circleCenter);
+
+            var circlePlane = new Plane(circleCenter, Vector3d.ZAxis);
+
+            var circle = new Circle(circlePlane, r);
+
+            DA.SetData(0, circle);
+        }
+
+        private double GetCircleXPosition(double angle, double centerOffset, double throatRadius)
         {
             //x = [a * c + sqrt(c ^ 2 + 1) * b] / (c ^ 2 + 1) or x = [a * c - sqrt(c ^ 2 + 1) * b] / (c ^ 2 + 1)
             //x = xp, a = T, b = D, c = tga
-        }
-
-        /// <summary>
-        /// Provides an Icon for the component.
-        /// </summary>
-        protected override System.Drawing.Bitmap Icon
-        {
-            get
-            {
-                //You can add image files to your project resources and access them like this:
-                // return Resources.IconForThisComponent;
-                return null;
-            }
-        }
-
-        /// <summary>
-        /// Gets the unique ID for this component. Do not change this ID after release.
-        /// </summary>
-        public override Guid ComponentGuid
-        {
-            get { return new Guid("DBA8CFCC-5611-4B78-A9A6-54A421E1FDFB"); }
+            var c = Math.Tan(Rhino.RhinoMath.ToRadians(angle));
+            var b = -centerOffset;
+            var a = throatRadius;
+            //var x = (a * c + Math.Sqrt(Math.Pow(c,2) + 1) * b) / (Math.Pow(c, 2) + 1);
+            var x = (a * c - Math.Sqrt(Math.Pow(c,2) + 1) * b) / (Math.Pow(c, 2) + 1);
+            return x;
         }
     }
 }
